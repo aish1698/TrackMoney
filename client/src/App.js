a8d3a641ac67e697d97dbab587a6fc35a11250bd
AISHWARYA PANI <paniaishwarya98@gmail.com>
2023-02-06 08:22:27 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'antd';
+import 'antd/dist/antd';
+function App() {
+  return (
+    <div className="App">
+      <h1>TRACK MONEY</h1>
+      <Button type="primary">Primary Button</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/DefaultLayout.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function DefaultLayout(props) {
+  return (
+    <div className='layout'>
+
+        <div className='header'>
+
+        </div>
+
+        <div className='content'>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default DefaultLayout
